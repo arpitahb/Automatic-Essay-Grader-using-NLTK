7e309a120018de3d2f4723c9cf70383da2679c30
Arpita <arpitahb2002@gmail.com>
2023-06-01 17:52:38 +0530

    first

---
+++ frontend/adminscript.js
@@ -0,0 +1,15 @@
+function navigateToDestination() {
+    const textarea1Value = document.getElementById("textbox").value;
+    const textarea2Value = document.getElementById("timelimit").value;
+  
+    if (textarea1Value.trim() !== "" && textarea2Value.trim() !== "") {
+       const destinationUrl = "candidate.html?textarea1Value=" + encodeURIComponent(textarea1Value);
+      
+      window.location.href = destinationUrl;
+    } else {
+      alert("Please fill in both text areas!");
+    }
+
+  }
+
+  
